c18efc44d101d247924dc96bfc9c0cca419e0867
Asif Afroj shayon <shayon.asif@gmail.com>
2024-06-12 23:10:46 +0600

    add --utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class AppColors{
+
+  static const Color primaryColor=Color(0xff9DFE00);
+  static const Color  secondaryColor=Color(0xff13D9E5);
+  static const Color baseColorBlack=Color(0xff191D29);
+  static const Color baseColorWhite=Color(0xffffffff);
+  static const Color baseColorGray=Color(0xffE3E3E3);
+  static const LinearGradient linearGradient=LinearGradient(colors: [
+    Color(0xff9DFE00),Color(0xff14D9E5),],
+    stops: [0.7, 0.7],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+}
---
+++ lib/utils/styles.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static const String fontFamily = 'PlusJakartaSans';
+
+  static  TextStyle caption1Strong (Color? color) {
+    return TextStyle(
+  fontFamily: fontFamily,
+  fontSize: 13.sp,
+  height: 1.38, // Line-height divided by font-size
+  fontWeight: FontWeight.w600,
+      color: color
+  );
+}
+
+  static  TextStyle body2 (Color? color) {
+   return TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 15.sp,
+      height: 1.33, // Line-height divided by font-size
+      fontWeight: FontWeight.w400,
+     color: color
+    );
+  }
+
+  static  TextStyle body2Strong (Color? color) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 15.sp,
+      height: 1.33, // Line-height divided by font-size
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static  TextStyle body1 (Color? color) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 17.sp,
+      height: 1.29, // Line-height divided by font-size
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle body1Strong(Color? color) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 17.sp,
+      height: 1.29, // Line-height divided by font-size
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle title3(Color? color) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 20.sp,
+      height: 1.25, // Line-height divided by font-size
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle title2(Color? color) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 22.sp,
+      height: 1.27, // Line-height divided by font-size
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle title1(Color? color) {
+    return TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 28.sp,
+      height: 1.21, // Line-height divided by font-size
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+}
