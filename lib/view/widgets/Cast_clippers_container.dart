fe700dd6a7a8ace3dbc38268b072a3702863d0e1
Asif Afroj shayon <shayon.asif@gmail.com>
2024-06-13 16:32:59 +0600

    update---view/widgets

---
+++ lib/view/widgets/Cast_clippers_container.dart
@@ -0,0 +1,31 @@
+//
+// import 'package:flutter/material.dart';
+//
+// class CustomContainerCast extends CustomClipper<Path>{
+//
+//   @override
+//   Path getClip(Size size)  {
+//
+//     Path path = Path();
+//     path.moveTo(size.width*0.0028220,size.height*0.9892297);
+//     path.lineTo(0,size.height*-0.0058534);
+//     path.lineTo(size.width*0.9989841,size.height*-0.0058534);
+//     path.quadraticBezierTo(size.width*1.0018061,size.height*0.4880590,size.width*1.0018061,size.height*0.6848513);
+//     path.quadraticBezierTo(size.width*0.8840163,size.height*0.7587801,size.width*0.5587538,size.height*0.9950831);
+//
+//    return path;
+//
+//
+//   }
+//
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
+//     return true;
+//   }
+//
+//   @override
+//   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+//     return true;
+//   }
+//
+// }
