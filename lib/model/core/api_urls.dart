4288bccb5c8a5c868ba966a85069af8fb4149e62
Asif Afroj shayon <shayon.asif@gmail.com>
2024-06-16 14:37:09 +0600

    update--lib/model

--- lib/model/core/api_urls.dart
+++ lib/model/core/api_urls.dart
@@ -1,5 +1,4 @@
-class ApiUrl{
-     String baseUrl="https://rickandmortyapi.com/api/";
-     String character="character";
-
+class ApiUrl {
+     static const String baseUrl = 'https://rickandmortyapi.com/api/';
+     static const String character = 'character';
 }
